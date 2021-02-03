5f303e1cad84102cb826ed53d1217fb96b86d5b7
YewonLimm <limyeng@naver.com>
2021-02-03 21:01:03 +0900

    영양 성분표시까지 얼추...

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,17 +2,62 @@ import React from 'react';
 import { inject, observer } from 'mobx-react';
 import { TextField, IconButton  } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
+import axios from 'axios';
+
+const getData = async (food) => {
+  return await axios.get(`http://openapi.foodsafetykorea.go.kr/api/dfa1a00161e84b9c912a/I2790/json/1/5/DESC_KOR=${food}`)
+}
 
 @inject('foodStore')
 @observer
 class Main extends React.Component{
 
+
+
   handleSubmit = (e) => {
     e.preventDefault();
     console.log(e.target.search.value)
-    this.props.foodStore.addFood(e.target.search.value)
+    getData(e.target.search.value).then(result => {
+
+      const correctFoods = result.data.I2790.row.filter((food) => food.DESC_KOR === e.target.search.value)
+      
+
+      if(!correctFoods.length){
+        console.log("정의안댐!")
+        return;
+      }
+
+      const {
+        GROUP_NAME,
+        DESC_KOR,
+        SUB_REF_NAME,
+        SERVING_SIZE,
+        NUTR_CONT1,
+        NUTR_CONT2,
+        NUTR_CONT3,
+        NUTR_CONT4,
+        NUTR_CONT5,
+        NUTR_CONT6
+      } = correctFoods[0]
+
+      this.props.foodStore.addFood({
+        group_name : GROUP_NAME,
+        food_name : DESC_KOR,
+        result_from : SUB_REF_NAME,
+        serving_size : SERVING_SIZE,
+        kcal : NUTR_CONT1,
+        carbo : NUTR_CONT2,
+        pro : NUTR_CONT3,
+        fat : NUTR_CONT4,
+        sugar : NUTR_CONT5,
+        salt :NUTR_CONT6
+      })
+    })
+    .catch(failed=>console.log(failed))
     this.props.changePage(1)
   }
+
+
 
   render(){
     return <div style={{
--- src/components/Nutrition.js
+++ src/components/Nutrition.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { inject, observer } from 'mobx-react';
 import { TextField, IconButton } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
+import '../styles/media.css'
 
 @inject('foodStore')
 @observer
@@ -14,29 +15,22 @@ class Nutrition extends React.Component {
 
   render() {
     return <div>
-      <h1 style={{}}>영양 성분</h1>
-      <hr />
-      <div style={{ display: 'flex' }}>
-        <div style={{ borderRight: "1px solid" }}>
-          <ul>
-            <li>
-              구이류
-            </li>
-            <li>
-              육류
-            </li>
-            <li>
-              김치류
-            </li>
-          </ul>
-        </div>
-        <ul>
+      <h1>영양 성분</h1>
+      <div style={{ display: 'flex', borderTop: "1px solid", flexWrap:'wrap', height: '300px', overflowY:'scroll',  overflowX:'hidden' }}>
           {this.props.foodStore.foods.map((food, index) => {
-            return <li key={index}>{food}</li>
+            return <div style={{ padding: 4, maxWidth:300, minWidth:200 ,width: '32%',height: '300px', borderBottom: '1px solid gray', overFlow: 'scroll', backgroundColor: 'lightblue'}} key={index} className="each_food">
+              <h2>{food.food_name}</h2>
+              <p>칼로리 : {food.kcal} kcal / {food.serving_size} g</p>
+              <p>탄수화물 : {food.carbo} g</p>
+              <p>단백질 : {food.pro} g</p>
+              <p>지방 : {food.fat} g</p>
+              <p>당 : {food.sugar} g</p>
+              <p>나트륨 : {food.salt} g</p>
+              {/* <p>출처 : {food.result_from}</p> */}
+              </div>
           })}
-        </ul>
       </div>
-      <IconButton onClick={() => this.props.changePage(0)} style={{ padding: 0 }}>
+      <IconButton onClick={() => this.props.changePage(0)} style={{ padding: 0, position:'fixed', top:16, right:16 }}>
         <SearchIcon style={{ width: 56, height: 56 }} />
       </IconButton>
     </div>
@@ -45,3 +39,5 @@ class Nutrition extends React.Component {
 }
 
 export default Nutrition
+
+//http://openapi.foodsafetykorea.go.kr/api/dfa1a00161e84b9c912a/I2790/json/1/5/DESC_KOR=가자미구이
